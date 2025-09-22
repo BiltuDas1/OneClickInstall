271a16186cf651dc8ab3a8c4dbd83a9cd34570b0
BiltuDas1 <billionto@gmail.com>
2025-09-23 00:46:08 +0530

    Now autobuild will build first and then enable autobuild service

--- autobuild.py
+++ autobuild.py
@@ -11,16 +11,20 @@ logger.setLevel(logging.INFO)
 stream = logging.StreamHandler()
 stream.setFormatter(colorlog.ColorFormatter("%(levelname)s %(message)s"))
 logger.addHandler(stream)
+BUILD_SCRIPT = "./scripts/build.sh"
 
 
 async def watch():
+  subprocess.run(["bash", BUILD_SCRIPT])
+  logger.info("Build Done")
+
   paths_to_watch = ["windows/"]
   logger.info("Autobuilding Process Started... watching for changes")
 
   try:
     async for _ in awatch(*paths_to_watch):
       logger.info("Changes detected, recompiling binary...")
-      subprocess.run(["bash", "./scripts/build.sh"])
+      subprocess.run(["bash", BUILD_SCRIPT])
       logger.info("Process Complete")
   except asyncio.exceptions.CancelledError:
     pass
